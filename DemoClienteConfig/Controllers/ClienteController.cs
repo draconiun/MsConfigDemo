5254dfb6edba2b9d0eff9d549e691fdd4b0bfbd8
draconiun <aaronmejiaalfaro@gmail.com>
2019-12-01 16:19:50 -0500

    Agregar archivos de proyecto.

---
+++ DemoClienteConfig/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using DemoClienteConfig.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DemoClienteConfig.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ParametroConfig options;
+
+        public ClienteController(IOptions<ParametroConfig> options)
+        {
+            this.options = options.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(options);
+        }
+    }
+}
---
+++ DemoClienteConfig/Model/ParametroConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoClienteConfig.Model
+{
+    public class ParametroConfig
+    {
+        public string usuario { get; set; }
+        public string clave { get; set; }
+        public string red { get; set; }
+    }
+}
